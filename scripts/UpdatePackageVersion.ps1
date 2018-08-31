7d3f3f172d23a55cbc99b7282fd44b0fc4f13eca
Matthias Mueller <matthiasm@live.de>
2018-08-31 23:23:21 +0200

    WIP

--- scripts/UpdatePackageVersion.ps1
+++ scripts/UpdatePackageVersion.ps1
@@ -4,12 +4,6 @@ param (
 
 function getPackageFiles() {
   $packageFiles = New-Object System.Collections.Generic.List[System.IO.FileInfo]
-
-  Write-Host 'I am here: ' $PSScriptRoot
-
-  # $mainPackage = Get-ChildItem -File ($PSScriptRoot + '/../*package.json')
-  # $packageFiles.Add($mainPackage)
-
   $libPackages = Get-ChildItem -File ($PSScriptRoot + '/../projects/package.json') -Recurse -Force
 
   foreach ($libPackage in $libPackages) {
@@ -38,17 +32,13 @@ $packageFiles = getPackageFiles
 
 foreach ($packageFile in $packageFiles) {
   Write-Host 'Updating' $packageFile
+
   $directoryName = [System.IO.Path]::GetDirectoryName($packageFiles);
-  Write-Host 'Directory' $$directoryName
   Set-Location $directoryName
 
-  # The git commands are needed, since version can only be updated on a clean git repo --> https://github.com/npm/npm/issues/6879
+  # Assure git folder is clean
   git add .
   git commit -am 'auto'
 
   npm version $buildVersion
-
-  # $packageContent = Get-Content $packageFile
-  # $replaced = $packageContent -replace '"version": "0.0.1"', $newVersionString
-  # Set-Content -Path $packageFile -value $replaced
 }
