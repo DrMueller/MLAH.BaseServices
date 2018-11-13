3ce88711db5666a6799cbc88f93bd1f0f642b5dd
Matthias Mueller <matthiasm@live.de>
2018-11-13 19:36:40 +0100

    WIP

--- projects/drmueller/ng-base-services/src/public_api.ts
+++ projects/drmueller/ng-base-services/src/public_api.ts
@@ -2,6 +2,6 @@
 //  * Public API Surface of base-services
 //  */
 
-export * from './lib/areas/http';
-export * from './lib/areas/object-creation';
+export * from './lib/areas/http/index';
+export * from './lib/areas/object-creation/index';
 export * from './lib/areas/ng-base-services.module';
