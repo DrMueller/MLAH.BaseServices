88b89a4141ebeb0a0d3238b8d7c2e6ba8d88c487
Matthias Mueller <matthiasm@live.de>
2018-12-21 22:01:17 +0100

    WIP

--- projects/drmueller/ng-base-services/src/public_api.ts
+++ projects/drmueller/ng-base-services/src/public_api.ts
@@ -7,5 +7,6 @@ export * from './lib/areas/http/http-base/index';
 export * from './lib/areas/object-creation/index';
 export * from './lib/areas/ng-base-services.module';
 export * from './lib/areas/http/in-memory-api/services';
+export * from './lib/areas/http/in-memory-api/models';
 export * from './lib/areas/local-storage/services';
 export * from './lib/areas/local-storage/models';
