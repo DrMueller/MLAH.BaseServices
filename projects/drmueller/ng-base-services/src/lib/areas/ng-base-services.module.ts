c5d142d5ac50b9edd55bedf4a5b7959c8362e8a8
Matthias Mueller <matthiasm@live.de>
2019-01-30 21:22:20 +0100

    WIP

--- projects/drmueller/ng-base-services/src/lib/areas/ng-base-services.module.ts
+++ projects/drmueller/ng-base-services/src/lib/areas/ng-base-services.module.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LocalHttpService } from './http/http-base/services/local-http.service';
+import { AppSettingsProviderService } from '@drmueller/ng-base-services/lib/areas/app-settings/services/app-settings-provider.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   providers: [
-    LocalHttpService
+    LocalHttpService,
+    AppSettingsProviderService
   ]
 })
 export class NgBaseServicesModule {
