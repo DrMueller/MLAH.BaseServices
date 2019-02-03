b34eebe332a3d6db3c86c36fe426e7ffbc5b23e5
Matthias Mueller <matthiasm@live.de>
2019-02-03 14:43:13 +0100

    WIP

--- projects/drmueller/ng-base-services/src/integration-tests/test-areas/http/services/http-base.service.spec.ts
+++ projects/drmueller/ng-base-services/src/integration-tests/test-areas/http/services/http-base.service.spec.ts
@@ -45,7 +45,7 @@ describe('TestHttpService', () => {
 
     // Assert
     it('Calls http client for deletion', () => {
-      expect(httpClientMock.delete).toHaveBeenCalled();
+      expect(httpClientMock.delete).toEqual(1);
     });
 
     it('Passes correct URL and options', () => {
